8d259a88c8c1907bbb9047c151c398cbcbd3cf74
sindhu023 <59205379+sindhu023@users.noreply.github.com>
2022-12-12 16:28:52 +0530

    Add files via upload

---
+++ assembly/codes/xor.asm
@@ -0,0 +1,38 @@
+;logical AND, OR and XOR operations
+;output displayed using 7447 IC
+
+.include "/home/student/m328Pdef.inc"
+
+
+ldi r17, 0b11111110 ; identifying input pins 8,9
+out DDRB,r17		; declaring pins as input
+ldi r17, 0b11111111 ;
+out PORTB,r17		; activating internal pullup for pins 8,9
+in r17,PINB
+
+ldi r16, 0b00000100 ;identifying output pins 2,3,4,5
+out DDRD,r16		;declaring pins as output
+
+
+
+ldi r16,0b00000000;initializing W
+ldi r17,0b00000001	;initializing X
+eor r16,r17			;X XOR X
+
+;following code is for displaying output
+;shifting LSB in r16 to 2nd position
+ldi r20, 0b00000010	;counter = 2
+
+rcall loopw		;calling the loopw routine
+
+out PORTD,r16		;writing output to pins 2,3,4,5
+
+
+Start:
+rjmp Start
+
+;loop for bit shifting
+loopw:	lsl r16			;left shift
+	dec r20			;counter --
+	brne	loopw	;if counter != 0
+	ret
